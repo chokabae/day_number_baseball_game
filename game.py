29359817b52696c51539d506f961056d0f0d9a7e
chokabae <92rkqo@naver.com>
2022-10-19 16:09:23 +0900

    입력 값을 3칸의 list로 분리
    - 각 자리를 구하는 공식 생성

--- game.py
+++ game.py
@@ -10,3 +10,25 @@ from random import sample
 cpu_numbers = sample(range(1, 10), 3)
 
 print(cpu_numbers)
+
+# 사람이 3자리 숫자를 입력 > 맞출때 까지 계속 입력
+# 한칸한칸 비교해야해서 list로 진행
+
+while True :
+    user_number = list()
+
+    # 3자리 숫자를 입력하면 > 3칸의 목록으로 분리 
+    input_num = int(input('3자리 숫자 입력 : '))
+
+    #ex. 123 => [1, 2, 3]으로 분리.
+    # 3자리 정수가 들어왔다고 전제.
+
+    # 제일 먼저 추가 : 100의 자리
+    # 그 다음 : 10의 자리
+    # 그 다음 : 1의 자리
+
+    user_number.append(input_num // 100) # 100의 자리
+    user_number.append(input_num // 10 % 10) # 10의 자리
+    user_number.append(input_num % 10) # 1의 자리
+
+    print(user_number)
